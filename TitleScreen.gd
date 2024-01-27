8474313f1c84a9268cc4a224837d583c99b01d4c
quietkiller4 <kevinkellerjr@gmail.com>
2024-01-27 12:58:57 -0500

    Options_Screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -5,7 +5,7 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://player.tscn")
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://options_screen.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
